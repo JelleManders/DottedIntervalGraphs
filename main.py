c62dc5f4de96ac0d204917e38434eb4217b779fe
JelleManders <jelle4life@gmail.com>
2017-11-21 15:27:35 +0100

    Introduced new test-case
    nx.draw still requires some changes to be made, adjacency matrix needs to be available somewhere.

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from DIG import DottedIntervalGraph as DIG
 
-G = DIG()
+def a123():
+	G = DIG()
+	for node, sequence in [("node1",(20,7,5)),("node2",(25,11,7)),("node3",(36,135,11))]:
+		G.add_sequence(node, sequence)
+	return G
 
-for node, sequence in [("node1",(20,7,5)),("node2",(25,11,7)),("node3",(36,135,11))]:
-	G.add_sequence(node, sequence)
+def c5():
+	G = DIG()
+	for node, sequence in [(0, (1,1,2)), (1, (2,1,2)), (2, (3,1,2)), (3, (4,1,2)), (4, (1,4,2))]:
+		G.add_sequence(node, sequence)
+	return G
 
-network = nx.Graph(G)
-print network.adj
+print("\n#####  nx.dodecahedral_graph:  #####\n")
+G = nx.dodecahedral_graph()
+nx.draw(G)
+
+
+print("\n#####  DIG.c5:  #####\n")
+G = c5();
+G.gen_adj()
+print(list(G.edges()))
+	# print(e)
+# print(G._adj['node2']['node3'].get(1, 1))
+nx.draw(G)
